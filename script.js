540f9c4baf9d85490adc7d0bc1ae381aa2926e00
VitalkaZu <vitalkazu@gmail.com>
2022-10-07 20:18:16 +0800

    delete console.log

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const listRepo = document.querySelector(".list-repo");
 async function loadData(name) {
     const resultLoad = await fetch(`https://api.github.com/search/repositories?q=${name}&sort=stars&order=desc&per_page=5`);
     const arrResult = await resultLoad.json();
-    console.log(arrResult);
     clear(autoComplete);
      const listSearch = document.createElement("ul");
      listSearch.classList.add("list-search");
@@ -15,7 +14,6 @@ async function loadData(name) {
                  rowSearch.classList.add("item-search");
                  rowSearch.textContent = item.name;
                  rowSearch.addEventListener("click",()=> {
-                     // console.log(item.stargazers_count);
                      clear(autoComplete);
                      searchInput.value = "";
                      addCardRepo(item.name, item.owner.login, item.stargazers_count);
@@ -37,7 +35,6 @@ searchInput.addEventListener("input", debouncePrintInputValue)
 function printInputValue () {
     if(searchInput.value) {
         loadData(searchInput.value);
-        console.log(searchInput.value);
     } else clear(autoComplete);
 }
 
